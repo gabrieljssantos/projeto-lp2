914ba83999027a51739f9e1a4c9a9b3af4b8b70b
gabrieljssantos <gabriel.jssantos003@gmail.com>
2024-06-13 17:38:27 -0300

    Inicio Config

---
+++ src/main/java/fatec/projeto/projeto_lp2/ProjetoLp2Application.java
@@ -0,0 +1,13 @@
+package fatec.projeto.projeto_lp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fatec/projeto/projeto_lp2/ProjetoLp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.projeto.projeto_lp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
